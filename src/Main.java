174227a1d22bae743ffaa870dd9121f7b31cc636
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-21 14:13:05 -0300

    Método String Intern.

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println(str01 == str02);
         /*
         * TRUE.
-        * IMPORTANTE: El operador == compara si es la MISMA POSICIÓN EN MEMORIA.
+        * IMPORTANTE: El operador == compara SI ESTÁN EN LA MISMA POSICIÓN EN MEMORIA.
         * Java al crear el string se fija en la lista de strings de la string pool y
         * si encuentra una coincidencia apunta hacia el mismo lugar en memoria.
         * Esto lo hace más rápido y eficiente.
@@ -16,7 +16,8 @@ public class Main {
         String str03 = new String("Agustin");
         System.out.println(str01 == str03);
         /*
-        * FALSE. Porque al crear el String con NEW. Es un objeto que no está en el
+        * FALSE.
+        * Porque al crear el String con NEW. Es un objeto que no está en el
         * STRING de la memoria. Está en el HEAP pero no en el STRING POOL.
         */
 
@@ -24,8 +25,22 @@ public class Main {
         /*
          * TRUE.
          * IMPORTANTE:
-         * El METODO EQUALS compara CONTENIDO independientemente de donde este ubicado
+         * El MÉTODO EQUALS compara CONTENIDO independientemente de donde este ubicado
          * en memoria ese String.
          */
+
+        String str04 = new String("Hola");
+        String str05 = "Hola";
+        System.out.println(str04==str05);
+        // FALSE.
+        str04 = str05.intern();
+        System.out.println(str04 == str05);
+        /*
+        * TRUE.
+        * Cuando el MÉTODO INTERN es llamado, se busca en el STRING POOL y si
+        * coincide con otro valor lo apunta. Si no lo consigue lo pone como una
+        * cadena más en la lista.
+        */
+
     }
 }
