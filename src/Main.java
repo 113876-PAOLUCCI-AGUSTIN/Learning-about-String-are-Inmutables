b6e4a28b0b249af4c0dc79dbde855132e4f3c29e
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-21 13:50:25 -0300

    Comparaciones con ==

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str01 = "Agustin";
+        String str02 = "Agustin";
+        System.out.println(str01 == str02);
+        /*
+        * TRUE.
+        * IMPORTANTE: El operador == compara si es la MISMA POSICIÓN EN MEMORIA.
+        * Java al crear el string se fija en la lista de strings de la string pool y
+        * si encuentra una coincidencia apunta hacia el mismo lugar en memoria.
+        * Esto lo hace más rápido y eficiente.
+        * El valor de ambas variables está en el STRING POOL DEL HEAP SPACE de la memoria y
+        * apuntan a un solo lugar.
+        */
+
+        String str03 = new String("Agustin");
+        System.out.println(str01 == str03);
+        /*
+        * FALSE. Porque al crear el String con NEW. Es un objeto que no está en el
+        * STRING de la memoria. Está en el HEAP pero no en el STRING POOL.
+        */
     }
 }
